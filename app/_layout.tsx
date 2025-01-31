771150e8fa2b287436eaeb813cb059b269269cfd
NNakreSS <serkanatmaca15@gmail.com>
2025-02-01 01:16:23 +0300

    feat: added & implement nativewind

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -10,6 +10,7 @@ import { useEffect } from "react";
 import "react-native-reanimated";
 import { useColorScheme } from "@/hooks/useColorScheme";
 import "../constants/i18n";
+import "./global.css";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
