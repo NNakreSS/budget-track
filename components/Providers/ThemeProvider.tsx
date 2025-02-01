bf586672ba0d576f0d1dd25466e9128000ae6c5b
NNakreSS <serkanatmaca15@gmail.com>
2025-02-01 16:04:19 +0300

    feat: Add ThemeProvider and color theme utilities for dynamic theming

---
+++ components/Providers/ThemeProvider.tsx
@@ -0,0 +1,34 @@
+import { themes } from "@/utils/color-theme";
+import { useColorScheme } from "nativewind";
+import { createContext, useContext } from "react";
+import { View } from "react-native";
+
+interface ThemeProviderProps {
+  children: React.ReactNode;
+}
+
+export const ThemeContext = createContext<{
+  theme: "light" | "dark" | undefined;
+}>({
+  theme: "light",
+});
+
+export default function ThemeProvider({ children }: ThemeProviderProps) {
+  const { colorScheme } = useColorScheme();
+
+  return (
+    <ThemeContext.Provider value={{ theme: colorScheme }}>
+      <View style={themes[colorScheme ?? "dark"]} className="flex-1">
+        {children}
+      </View>
+    </ThemeContext.Provider>
+  );
+}
+
+export const useTheme = () => {
+  const { theme } = useContext(ThemeContext);
+  if (!theme) {
+    throw new Error("useTheme must be used within a ThemeProvider");
+  }
+  return theme;
+};
